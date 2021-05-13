34015026b9d8b5895e341c31d52ed8fcb98488e7
SajadS555 <83949033+SajadS555@users.noreply.github.com>
2021-05-13 16:42:46 +0530

    Create day-3 task-2.c

---
+++ day-3 task-2.c
@@ -0,0 +1,14 @@
+//Write a C Program to convert Fahrenheit to Celsius.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+   float f;
+   scanf("%f",&f);
+   printf("%.2f",((f-32)*5)/9);
+    return 0;
+}
