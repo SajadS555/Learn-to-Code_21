0958f014ae82a06269ad3f75789b9a77d9aedfaf
SajadS555 <83949033+SajadS555@users.noreply.github.com>
2021-05-15 19:34:15 +0530

    Create day4 task1.c

---
+++ day4 task1.c
@@ -0,0 +1,26 @@
+//Write a C Program to verify if a given year is a leap year or no
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int a;
+    scanf("%d",&a);
+    if(a%400==0)
+    
+        printf("Yes");
+    else if(a%4==0)
+    {
+       if(a%100==0)
+            printf("No");
+    
+     else printf("Yes");
+    }
+    else
+        printf("No");
+    
+    
+    return 0;
+}
