a3ec667b2e0c940d0c5c54d6bd189f734f52b33c
SajadS555 <83949033+SajadS555@users.noreply.github.com>
2021-06-09 09:53:20 +0530

    Create day20 task1.c

---
+++ day20 task1.c
@@ -0,0 +1,31 @@
+//Write a C program to find the factorial of a given number using a recursive function
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()  
+  {
+      int n,a;
+      scanf("%d",&n);
+      if(n<0){
+          printf("ERROR");
+      }
+       else
+       {
+            a=factorial(n);
+            printf("%d",a);
+       }
+   }
+
+    int factorial(int n)
+    {
+        int a;
+        if(n==0)
+            a=1;
+        else
+            a=n*factorial(n-1);
+        return a;
+    }
