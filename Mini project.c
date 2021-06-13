b914ff410ebae98a81ad8dbdef1440f81d43b9f4
SajadS555 <83949033+SajadS555@users.noreply.github.com>
2021-06-13 20:23:40 +0530

    Update Mini project.c

--- Mini project.c
+++ Mini project.c
@@ -1,4 +1,4 @@
-// program to read 5 contact and display it
+// program to read  contact and display it
 
 #include <stdio.h>
 #include <string.h>
@@ -13,13 +13,16 @@ int main()
 {
     struct contact person[5];
     
-    int i,x;
+    int i,x,n;
   label1: printf("1.Add contact\n2.Display contact\n");
 	
 	scanf("%d", &x);
+
   
   if(x==1){ 
-    for (i = 0; i < 5; i++)
+    printf("Enter the numer of contacts to add:>\n");
+    scanf("%d",&n);
+    for (i = 0; i < n; i++)
     {
         printf("Enter the name: ");
         scanf("%s", person[i].name);
@@ -30,7 +33,7 @@ int main()
     goto label1;
     }
      else if(x==2){ 
-               for (i = 0; i < 5; i++)
+               for (i = 0; i < n; i++)
             {
                        printf("%d.", i+1);
                        printf("%s", person[i].name);
